6e46cc7a53300da1eb3246e1f5fbc2fd4f05d90b
sliitguy <it23259416@my.sliit.lk>
2025-10-26 11:23:04 +0530

    updated initial files

---
+++ restuarant-service/src/main/java/com/nivakaran/restuarantservice/RestuarantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nivakaran.restuarantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuarantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestuarantServiceApplication.class, args);
+    }
+
+}
